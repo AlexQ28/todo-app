4e7fca2dd6ca0246003b464a04b0f7f770f55c99
AlexQ28 <aquezada010@gmail.com>
2020-01-29 19:21:15 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react'; //Hooks almacenar estados
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/*
+function Greeting(props){
+  console.log(props.name);  
+  return <h1>Hola {props.name} {props.lastname} tienes {props.age}  </h1>;
+}
+*/
+
+//Componente antiguo 
+class Greeting extends React.Component {
+  render(){  //Metodo que manda llamar React se llama render 
+    return <h1>Hola {this.props.name} {this.props.lastname} tienes {this.props.age} </h1>;
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      lastname: '',
+      age: '',
+    }
+  }
+
+handleChange = event => {
+    this.setState({
+    name: event.target.value
+ });
+}
+
+render(){
+   return (
+    <div>
+       <input type="text" onChange= {this.handleChange}/>
+       <input type="text" onChange= {this.handleChange2}/>
+       <input type="text" onChange= {this.handleChange3}/>
+       <Greeting name={this.state.name} lastname={this.state.lastname} age={this.state.age}/>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
+
+//Componente Principal
+/*
+function App() {
+  const [name, setName] = useState(""); //->[valorActual, funcionParaActual]
+  const handleChange = event => {
+    setName(event.target.value);
+  }
+  const [lastname, setlastName] = useState("");
+  const handleChange2 = event => {
+    setlastName(event.target.value);
+ }
+  const [age, setAge]=useState("");
+  const handleChange3 = event => {
+    setAge(event.target.value); 
+ }
+  /*JSX*/
+/*  
+  return (
+      <div>
+         <input type="text" onChange= {handleChange}/>
+         <input type="text" onChange= {handleChange2}/>
+         <input type="text" onChange= {handleChange3}/>
+         <Greeting name={name} lastname={lastname} age={age}/>
+      </div>
+  );
+}
+*/
+
+
+
